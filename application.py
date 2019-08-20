0962867b3d195cfa206ed25a7168cf1002f9a57e
Steffan Riemann Hansen <steffan.r.hansen@siemensgamesa.com>
2019-08-20 08:55:37 +0200

    changed hello world flask app to hello world flask api

--- application.py
+++ application.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+"""
+Exposing of the pythia api
+"""
 
-@app.route("/")
-def hello():
-    return "Hello World! :)"
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_jsonpify import jsonify
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class TestClass1(Resource):
+    def get(self):
+        return {'Hello': 'World! test 1'}
+
+
+class TestClass2(Resource):
+    def get(self):
+        return {'Hello': 'World! test 2'}
+
+
+api.add_resource(TestClass1, '/test1')
+api.add_resource(TestClass2, '/test2')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ azure_connection.py
@@ -0,0 +1,4 @@
+"""
+account_name='tcmdatablob'
+account_key='Gvue/WaE18KgR72SotQvABGJC8qkhZ9U6xvLqF7GvMt+o9wl/fsBenPAgD0r5PtysI7GJHNs5a2Ed7Xzvdx7Mw=='
+"""
